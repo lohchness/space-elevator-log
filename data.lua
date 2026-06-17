b00c50911df3467a715dc7d14c5e9a5059a91bf4
unknown <lohchness@gmail.com>
2026-06-17 08:02:11 +0100

    make space elevator icon available at runtime

--- data.lua
+++ data.lua
@@ -1,8 +1,15 @@
 data:extend {
-  {
-      type = 'custom-input',
-      name = 'space-log-open',
-      key_sequence = 'CONTROL + H',
-      enabled_while_spectating = true,
-  },
+    {
+        type = 'custom-input',
+        name = 'space-log-open-custom-input',
+        key_sequence = 'CONTROL + H',
+        enabled_while_spectating = true,
+    },
+
+    {
+        type = "sprite",
+        name = "space-elevator-log-gui-button",
+        filename = "__space-exploration-graphics-5__/graphics/icons/space-elevator.png",
+        size = 64,
+    }
 }
