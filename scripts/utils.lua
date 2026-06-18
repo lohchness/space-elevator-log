ee0601a789a7577cbc4ac340cbb59dab3f88de50
unknown <lohchness@gmail.com>
2026-06-18 09:22:14 +0100

    add table find function

--- scripts/utils.lua
+++ scripts/utils.lua
@@ -1,4 +1,4 @@
---- iniital word letters uppercase ('title case').
+--- initial word letters uppercase ('title case').
 -- Here 'words' mean chunks of non-space characters.
 ---@param s string
 ---@return string a string with each word's first letter uppercase
@@ -8,6 +8,19 @@ local function title(s)
     end))
 end
 
+--- Given a candidate search function, iterates over the table, calling the function
+-- for each element in the table, and returns the first element the search function returned true.
+-- Passes the index as second argument to the function.
+---@param tbl table             the table to be searched
+---@param func function         the function to use when searching for any matching element
+---@param ... any?
+---@return any, any|nil|?       the first found value
+local function find(tbl, func, ...)
+    for k, v in pairs(tbl) do if func(v, k, ...) then return v, k end end
+    return nil
+end
+
 return {
-    title = title
+    title = title,
+    find = find
 }
