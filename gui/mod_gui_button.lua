561868b01befbb2e91427ae296b63ec863dd15a7
unknown <lohchness@gmail.com>
2026-08-01 13:35:41 +0100

    Add handlers for mod gui button

--- gui/mod_gui_button.lua
+++ gui/mod_gui_button.lua
@@ -5,10 +5,25 @@ local constants = require("scripts/constants")
 
 --- This must be loaded with require() AFTER main_gui for gui_handlers.mod_gui_button_click
 
+
 ---@param player LuaPlayer
-local function add_mod_gui_button(player)
+local function remove_mod_gui_button(player)
     local flow = mod_gui.get_button_flow(player)
 
+    for _, i in pairs(flow.children) do
+        if i.name == constants.button_name then
+            i.destroy()
+        end
+    end
+end
+
+---@param player LuaPlayer
+local function add_mod_gui_button(player)
+    if not player.force.technologies[constants.unlock_tech_name].researched then
+        remove_mod_gui_button(player)
+    end
+
+    local flow = mod_gui.get_button_flow(player)
     if flow[constants.button_name] then return end
 
     flib_gui.add(
@@ -25,17 +40,39 @@ local function add_mod_gui_button(player)
 end
 
 ---@param player LuaPlayer
-local function remove_mod_gui_button(player)
-    local flow = mod_gui.get_button_flow(player)
+local function refresh_mod_gui_button(player)
+    remove_mod_gui_button(player)
+    add_mod_gui_button(player)
+end
 
-    for _, i in pairs(flow.children) do
-        if i.name == constants.button_name then
-            i.destroy()
-        end
+local function bulk_refresh_mod_gui_button()
+    for _, player in game.players do
+        refresh_mod_gui_button(player)
     end
 end
+
+---@param event EventData.on_player_joined_game
+script.on_event(defines.events.on_player_joined_game, function(event)
+    refresh_mod_gui_button(game.players[event.player_index])
+end)
+
+---@param event EventData.on_research_finished
+script.on_event(defines.events.on_research_finished, function(event)
+    if event.research.name then
+        bulk_refresh_mod_gui_button()
+    end
+end)
+
+---@param event EventData.on_research_reversed
+script.on_event(defines.events.on_research_reversed, function(event)
+    if event.research.name then
+        bulk_refresh_mod_gui_button()
+    end
+end)
 
 return {
     add_mod_gui_button = add_mod_gui_button,
     remove_mod_gui_button = remove_mod_gui_button,
+    refresh_mod_gui_button = refresh_mod_gui_button,
+    bulk_refresh_mod_gui_button = bulk_refresh_mod_gui_button,
 }
